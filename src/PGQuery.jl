212d88e8da9936cda2931a85b979dd243862c981
Mohammad Dashti <mdashti@gmail.com>
2019-01-03 20:46:42 +0330

    Added some substance to the project for loading the wrapper and exporting them.

--- src/PGQuery.jl
+++ src/PGQuery.jl
@@ -1,3 +1,32 @@
 module PGQuery
 
+import Libdl
+
+# Load in `deps.jl`, complaining if it does not exist
+const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
+if !isfile(depsjl_path)
+    error("PGQuery was not build properly. Please run Pkg.build(\"PGQuery\").")
+end
+
+# Module initialization function
+function __init__()
+    include(depsjl_path)
+end
+
+include(joinpath(@__DIR__, "gen", "CEnum.jl"))
+using .CEnum
+
+include(joinpath(@__DIR__, "gen", "ctypes.jl"))
+export Ctm, Ctime_t, Cclock_t
+
+include(joinpath(@__DIR__, "gen", "libpg_common.jl"))
+include(joinpath(@__DIR__, "gen", "libpg_api.jl"))
+
+# export everything with "pg_query_" prefix
+foreach(names(@__MODULE__, all=true)) do s
+   if startswith(string(s), "pg_query_")
+       @eval export $s
+   end
+end
+
 end # module
