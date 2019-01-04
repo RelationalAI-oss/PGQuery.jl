ea50761d79304cd65d80bc73839f751afbda1e22
Mohammad Dashti <mdashti@gmail.com>
2019-01-04 18:38:35 +0330

    Added `parse_sql_select` and `parse_sql` for parsing SQL queries.

--- src/PGQuery.jl
+++ src/PGQuery.jl
@@ -1,6 +1,7 @@
 module PGQuery
 
 import Libdl
+import JSON: parse
 
 # Load in `deps.jl`, complaining if it does not exist
 const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
@@ -28,5 +29,28 @@ foreach(names(@__MODULE__, all=true)) do s
        @eval export $s
    end
 end
+
+function parse_sql_select(input)
+    parsed_res = pg_query_parse(input)
+    if parsed_res.error == C_NULL
+        parse_tree = unsafe_string(parsed_res.parse_tree)
+        json_parse_tree = parse(parse_tree)
+        @assert length(json_parse_tree) == 1
+        first_exp = json_parse_tree[1]
+        @assert length(first_exp) == 1
+        @assert haskey(first_exp, "RawStmt")
+        raw_stmt = first_exp["RawStmt"]
+        @assert haskey(raw_stmt, "stmt")
+        stmt = raw_stmt["stmt"]
+        @assert haskey(stmt, "SelectStmt")
+        return stmt["SelectStmt"]
+    else
+        throw(error(unsafe_string(unsafe_load(parsed_res.error).message)))
+    end
+end
+
+parse_sql(input) = parse_sql_select(input)
+
+export parse_sql, parse_sql_select
 
 end # module
